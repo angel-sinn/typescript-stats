dab0ad9fdf2fa888e8638a7c18b52d59a5ec832a
Angel <angelkysinn@gmail.com>
2021-04-30 20:56:04 -0700

    Extract data from csv file and add tuple for data

---
+++ src/MatchResult.ts
@@ -0,0 +1,7 @@
+// fix set of possible outcomes
+// enum - enumeration -> signal to others that this is a collection of closely related values known at2 compile time
+export enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
-
-// fix set of possible outcomes
-// enum - enumeration -> signal to others that this is a collection of closely related values known at2 compile time
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
