05259046dc31e4d2b1a2a7dac6dec0b7c452b057
Angel <angelkysinn@gmail.com>
2021-04-30 21:41:23 -0700

    Refactor code and add generic for more reusable code

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,17 +1,13 @@
 import fs from 'fs';
-import { MatchResult } from './MatchResult';
 
-// tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-// mark class as abstract
-export abstract class CsvFileReader {
-  data: MatchData[] = [];
+// mark class as abstract; for reusability, use generic <T> for data type
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
 
   // mark method as abstract
-  abstract mapRow(row: string[]): MatchData;
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
